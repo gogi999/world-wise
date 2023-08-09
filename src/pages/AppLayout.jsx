ae2d65aaec75d7201d3dcdc22062536173679f3e
gogi999 <g.petrovic9@gmail.com>
2023-08-09 11:24:09 +0200

    Added useReducer hook and FakeAuthContext

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Map from '../components/Map';
 import Sidebar from '../components/Sidebar';
+import User from '../components/User';
 import styles from './AppLayout.module.css';
 
 const AppLayout = () => {
@@ -9,6 +10,7 @@ const AppLayout = () => {
         <div className={styles.app}>
             <Sidebar />
             <Map />
+            <User />
         </div>
     );
 }
